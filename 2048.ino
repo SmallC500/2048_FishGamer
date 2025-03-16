0e50eaea0c865e1ea8dd2e489f666d4401907ea6
SmallC500 <125797988+SmallC500@users.noreply.github.com>
2025-03-16 18:22:22 +0800

    Update 2048.ino
    Applied a little correction to adapt to the latest version of the library ArduinoJson

--- 2048.ino
+++ 2048.ino
@@ -1,6 +1,13 @@
+#include "FS.h"
+//This empty line is vital for compiling
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
 #include <XPT2046_Touchscreen.h>
+
+TaskHandle_t Task0;
+TaskHandle_t Task1;
 
 // Screen dimensions and game variables
 #define SCREEN_WIDTH 240
@@ -8,6 +15,8 @@
 #define TILE_SIZE 50
 #define GRID_SIZE 4
 #define TILE_MARGIN 8
+
+#define SAVINGS_JSON "/savings.json"
 
 #define T_CS 5
 #define T_IRQ 16
@@ -25,69 +34,116 @@
 TFT_eSPI tft = TFT_eSPI();  // Create TFT object
 XPT2046_Touchscreen ts(T_CS,T_IRQ);
 
-int grid[GRID_SIZE][GRID_SIZE]={
-  {0,
-  0,
-  0,
-  0},
-    {0,
-    0,
-    0,
-    0},
-      {0,
-      0,
-      0,
-      0},
-        {0,
+JsonDocument saves;
+char savedJson[250]="";
+/*The form of saved Json:
+{ "saved_grid":[[0,0,0,0],
+				[0,0,0,0],
+				[0,0,0,0],
+				[0,0,0,0]],
+	"saved_score":0,
+	"record_score":0,
+  "calibrated":false
+	"calData":[5000,5000,0,0]
+}*/
+
+int grid[GRID_SIZE][GRID_SIZE]={0};/*{
+  {2,
+  4,
+  8,
+  16},
+    {32,
+    64,
+    128,
+    256},
+      {512,
+      1024,
+      2048,
+      4096},
+        {8192,
         0,
         0,
         0}
-};
+};*/
 int colours[12]={TFT_DARKGREY,TFT_YELLOW,TFT_GREEN,TFT_OLIVE,TFT_CYAN,TFT_DARKCYAN,TFT_BLUE,TFT_NAVY,TFT_MAGENTA,TFT_PURPLE,TFT_MAROON,TFT_RED};
 int score = 0;
 int16_t calData[4]={5000/*X_MIN*/,5000/*Y_MIN*/,0/*X_MAX*/,0/*Y_MAX*/};
 bool valid=1;
+bool calibrated=0;
 bool gameOver=0;
+int moveDirection=-1;
 
 void touchCalibrate(int16_t (*calibP)[4], int confirmPin){
   pinMode(confirmPin,INPUT);
-  tft.drawCentreString("Calibrating",tft.width()/2,tft.height()/2,2);
-  tft.drawCentreString("XMIN:",tft.width()/2-15,tft.height()/2-15,2);
-  tft.drawCentreString("YMIN:",tft.width()/2-15,tft.height()/2-30,2);
-  while(digitalRead(confirmPin)){
-    vTaskDelay(1);
-    if(ts.tirqTouched()||ts.touched()){
-      TS_Point p=ts.getPoint();
-      if(p.x<*(*calibP)&&p.x!=0) *(*calibP)=p.x;
-      if(p.y<*(*calibP+1)&&p.y!=0) *(*calibP+1)=p.y;  
-    }
-    tft.setCursor(tft.width()/2+5,tft.height()/2-10);
-    tft.printf("%d ",*(*calibP));
-    tft.setCursor(tft.width()/2+5,tft.height()/2-25);
-    tft.printf("%d ",*(*calibP+1));
-  }
-  tft.setCursor(tft.width()/2+5,tft.height()/2-10);
-  tft.print("    ");
-  tft.setCursor(tft.width()/2+5,tft.height()/2-25);
-  tft.printf("    ");
-  while(!digitalRead(confirmPin))vTaskDelay(1);
+  DeserializationError error=deserializeJson(saves,savedJson);
+  if(!error){
+    calibrated=saves["calibrated"];
+    if(calibrated){
+      for(int i=0;i<4;i++)calData[i]=saves["calData"][i];
+    }else{
+      tft.drawCentreString("Calibrating",tft.width()/2,tft.height()/2,2);
+      tft.drawCentreString("XMIN:",tft.width()/2-15,tft.height()/2-15,2);
+      tft.drawCentreString("YMIN:",tft.width()/2-15,tft.height()/2-30,2);
+      while(digitalRead(confirmPin)){
+        vTaskDelay(1);
+        if(ts.tirqTouched()||ts.touched()){
+          TS_Point p=ts.getPoint();
+          if(p.x<*(*calibP)&&p.x!=0) *(*calibP)=p.x;
+          if(p.y<*(*calibP+1)&&p.y!=0) *(*calibP+1)=p.y;  
+        }
+        tft.setCursor(tft.width()/2+5,tft.height()/2-10);
+        tft.printf("%d ",*(*calibP));
+        tft.setCursor(tft.width()/2+5,tft.height()/2-25);
+        tft.printf("%d ",*(*calibP+1));
+      }
+      tft.setCursor(tft.width()/2+5,tft.height()/2-10);
+      tft.print("    ");
+      tft.setCursor(tft.width()/2+5,tft.height()/2-25);
+      tft.printf("    ");
+      while(!digitalRead(confirmPin))vTaskDelay(1);
 
-  tft.drawCentreString("XMAX:",tft.width()/2-15,tft.height()/2-15,2);
-  tft.drawCentreString("YMAX:",tft.width()/2-15,tft.height()/2-30,2);
-  while(digitalRead(confirmPin)){
-    vTaskDelay(1);
-    if(ts.tirqTouched()||ts.touched()){
-      TS_Point p=ts.getPoint();
-      if(p.x>*(*calibP+2)/*&&p.x!=0*/) *(*calibP+2)=p.x;
-      if(p.y>*(*calibP+3)/*&&p.y!=0*/) *(*calibP+3)=p.y;  
-    }
-    tft.setCursor(tft.width()/2+5,tft.height()/2-10);
-    tft.printf("%d ",*(*calibP+2));
-    tft.setCursor(tft.width()/2+5,tft.height()/2-25);
-    tft.printf("%d ",*(*calibP+3));
+      tft.drawCentreString("XMAX:",tft.width()/2-15,tft.height()/2-15,2);
+      tft.drawCentreString("YMAX:",tft.width()/2-15,tft.height()/2-30,2);
+      while(digitalRead(confirmPin)){
+        vTaskDelay(1);
+        if(ts.tirqTouched()||ts.touched()){
+          TS_Point p=ts.getPoint();
+          if(p.x>*(*calibP+2)/*&&p.x!=0*/) *(*calibP+2)=p.x;
+          if(p.y>*(*calibP+3)/*&&p.y!=0*/) *(*calibP+3)=p.y;  
+        }
+        tft.setCursor(tft.width()/2+5,tft.height()/2-10);
+        tft.printf("%d ",*(*calibP+2));
+        tft.setCursor(tft.width()/2+5,tft.height()/2-25);
+        tft.printf("%d ",*(*calibP+3));
+      }
+      //calibrated=1;
+      while(!digitalRead(confirmPin))vTaskDelay(1);
+      for(int i=0;i<4;i++)saves["calData"][i]=calData[i];
+      calibrated=1;
+      saves["calibrated"]=1;
+      saveStorage();
+    } 
+  } 
+}
+
+void saveStorage(){
+  serializeJson(saves,savedJson);
+  File f=SPIFFS.open(SAVINGS_JSON,"w");
+  if(f){
+    f.write((const unsigned char *)savedJson, sizeof savedJson);
+    f.close();
   }
-  //calibrated=1;
-  while(!digitalRead(confirmPin))vTaskDelay(1);
+}
+
+void saveGrid(){
+  JsonArray savedGrid=saves["saved_grid"].to<JsonArray>();
+  for(int x=0;x<GRID_SIZE;x++){
+    for(int y=0;y<GRID_SIZE;y++){
+      savedGrid[x][y]=grid[x][y];
+    }
+  }
+  saves["saved_score"]=score;
+  saveStorage();
 }
 
 void buttonSetup(){
@@ -99,33 +155,6 @@ void buttonSetup(){
   pinMode(R_DOWN,INPUT);
   pinMode(R_LEFT,INPUT);
   pinMode(R_RIGHT,INPUT);
-}
-
-void setup() {
-  Serial.begin(115200);
-  buttonSetup();
-  tft.init();
-  ts.begin();
-  tft.setRotation(0);  // Adjust rotation as needed
-  ts.setRotation(2-tft.getRotation()%2);
-  tft.fillScreen(TFT_BLACK);
-  touchCalibrate(&calData,L_UP);
-  tft.fillScreen(TFT_WHITE);
-
-  randomSeed(analogRead(13)); // Seed for random tile placement
-  startGame();
-}
-
-void loop() {
-  if (detectSwipe()) {
-    drawGrid();
-    if(gameOver)drawGameOver();
-  }
-  if(gameOver){
-    if(!digitalRead(L_UP)){
-      startGame();
-    }
-  }
 }
 
 int getSlideDirection(){
@@ -141,7 +170,7 @@ int getSlideDirection(){
         y2=map(p.y,calData[1],calData[3],0,tft.height());
       }
     }
-    if(abs(x2-x1)+abs(y2-y1)>50&&p.z<2000){
+    if(abs(x2-x1)+abs(y2-y1)>75&&p.z<1500){
       if(abs(x2-x1)>=abs(y2-y1)){
         if(x2>=x1) return 1; //right
         else return 3; //left
@@ -156,49 +185,55 @@ int getSlideDirection(){
 }
 
 // Function to start a new game
-void startGame() {
+void newGame() {
   memset(grid, 0, sizeof(grid));
   valid=1;
   score = 0;
   addRandomTile();
   addRandomTile();
   drawGrid();
+  tft.setCursor(10, 10);
+  tft.print("Score: ");
+  tft.print(score);
   valid=0;
   gameOver=0;
 }
 
-// Function to detect swipe gestures on the touchscreen
-bool detectSwipe() {
-  // Simulate swipe detection for demonstration
-  // Replace with real touchscreen detection code
-  // Implement touch position reading and determine swipe direction
-  int swipeDirection = getSlideDirection();
-  if(!digitalRead(R_UP)){
-    swipeDirection=0;
-    while(!digitalRead(R_UP))delay(1);
-  }else if(!digitalRead(R_DOWN)){
-    swipeDirection=2;
-    while(!digitalRead(R_DOWN))delay(1);
-  }else if(!digitalRead(R_LEFT)){
-    swipeDirection=3;
-    while(!digitalRead(R_LEFT))delay(1);
-  }else if(!digitalRead(R_RIGHT)){
-    swipeDirection=1;
-    while(!digitalRead(R_RIGHT))delay(1);
+void proceedGame(){
+  bool empty=1;
+  for(int x=0;x<GRID_SIZE;x++){
+    for(int y=0;y<GRID_SIZE;y++){
+      int processingSlot=saves["saved_grid"][x][y];
+      if(processingSlot>0){
+        grid[x][y]=saves["saved_grid"][x][y]; 
+        empty=0;
+      }
+    }
   }
-  /*if(Serial.available()){
-    String msg=Serial.readString();
-    swipeDirection=msg.toInt();
-  }*/
+  if(empty){
+    newGame();
+    saveGrid();
+  }else{
+    score=saves["saved_score"];
+    valid=1;
+    drawGrid();
+    tft.setCursor(10, 10);
+    tft.print("Score: ");
+    tft.print(score);
+    valid=0;
+    gameOver=0;
+  }
   
-  // Here, we will assume swipe direction is returned: 0 = up, 1 = right, 2 = down, 3 = left.
-  
+}
 
-  if (swipeDirection >= 0) {
-    moveTiles(swipeDirection);
-    return true;
-  }
-  return false;
+void updateRecord(){
+  saves["record_score"]=score;
+  saveStorage();
+}
+
+void resetStorage(){
+  SPIFFS.remove(SAVINGS_JSON);
+  ESP.restart();
 }
 
 // Function to add a random tile (2 or 4) at an empty location
@@ -253,6 +288,9 @@ void drawGrid() {
       drawTile(x, y);
     }
   }
+  tft.setCursor(10, 290);
+  tft.print("Record: ");
+  tft.print((int)saves["record_score"]);
 }
 
 // Function to draw a single tile
@@ -427,7 +465,6 @@ void moveTiles(int direction) {
     default:
       break;
   }
-  addRandomTile();  // Add a random tile after each move
 }
 
 /*/ Function to check if the game is over (no more moves)
@@ -445,3 +482,122 @@ void drawGameOver() {
   tft.drawString("Game Over", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 }
 
+void InitStorage(){
+  if (!SPIFFS.begin()) {
+    Serial.println("formatting file system");
+    SPIFFS.format();
+    SPIFFS.begin();
+  }
+  if(SPIFFS.exists(SAVINGS_JSON)){
+    File f=SPIFFS.open(SAVINGS_JSON,"r");
+    if(f&&f.size()==sizeof savedJson){
+      f.readBytes((char *)savedJson, f.size());
+      Serial.println(savedJson);
+      f.close();
+    }else{
+      f.close();
+      Serial.println("Savings are broken");
+      SPIFFS.remove(SAVINGS_JSON);
+      ESP.restart();
+    }
+  }else{
+    strcpy(savedJson,"{\"saved_grid\":[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],\"saved_score\":0,\"record_score\":0,\"calibrated\":0,\"calData\":[5000,5000,0,0]}");
+    File f = SPIFFS.open(SAVINGS_JSON, "w");
+    if (f) {
+      f.write((const unsigned char *)savedJson, sizeof savedJson);
+      f.close();
+      ESP.restart();
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  xTaskCreatePinnedToCore(
+                    Task0code,   /* Task function. */
+                    "Task0",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task0,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */                  
+  delay(500); 
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    1);          /* pin task to core 1 */
+  delay(500); 
+}
+
+void Task0code(void * pvParameters){
+  tft.init();
+  tft.setRotation(0);  // Adjust rotation as needed
+  ts.setRotation(2-tft.getRotation()%2);
+  tft.fillScreen(TFT_BLACK);
+  InitStorage();
+  touchCalibrate(&calData,L_UP);
+  tft.fillScreen(TFT_WHITE);
+  randomSeed(analogRead(13)); // Seed for random tile placement
+  proceedGame();
+  for(;;){
+    vTaskDelay(1);
+    if(moveDirection>=0){
+      moveTiles(moveDirection);
+      moveDirection=-1;
+      addRandomTile();
+      drawGrid();
+      if(gameOver){
+        drawGameOver();
+        if(score>saves["record_score"])updateRecord();
+      }
+    }
+  }
+}
+
+void Task1code(void * pvParameters){
+  buttonSetup();
+  ts.begin();
+  bool pressed=0;
+  for(;;){
+    vTaskDelay(1);
+    if(calibrated){
+      if(!gameOver){
+        moveDirection=getSlideDirection();
+        if(!digitalRead(R_UP)&&!pressed){
+          moveDirection=0;
+          pressed=1;
+        }else if(!digitalRead(R_DOWN)&&!pressed){
+          moveDirection=2;
+          pressed=1;
+        }else if(!digitalRead(R_LEFT)&&!pressed){
+          moveDirection=3;
+          pressed=1;
+        }else if(!digitalRead(R_RIGHT)&&!pressed){
+          moveDirection=1;
+          pressed=1;
+        }else if(!digitalRead(L_LEFT)&&!pressed){
+          pressed=1;
+          tft.fillScreen(TFT_GREEN);
+          saveGrid();
+          drawGrid();
+        }else if(!digitalRead(L_DOWN)&&!pressed){
+          resetStorage();
+        } 
+      }
+      if(!digitalRead(L_UP)&&!pressed){
+        newGame();
+        pressed=1;
+      }
+    }
+    if(digitalRead(R_UP)&&digitalRead(R_DOWN)&&digitalRead(R_LEFT)&&digitalRead(R_RIGHT)&&digitalRead(L_DOWN)&&digitalRead(L_UP)&&digitalRead(L_LEFT))pressed=0;
+  }
+}
+
+void loop() {
+  
+}
